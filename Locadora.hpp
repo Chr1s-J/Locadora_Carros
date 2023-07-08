cb6f690ce91547ae98e5b89c59109f586e9bf0da
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-07 21:58:18 -0300

    Update Locadora.hpp

--- Locadora.hpp
+++ Locadora.hpp
@@ -1,5 +1,10 @@
-#ifndef LOCADORA_H
-#define LOCADORA_H
+/**
+ * @file Locadora.hpp
+ * @brief Definição da classe Locadora
+ */
+
+#ifndef LOCADORA_HPP
+#define LOCADORA_HPP
 
 #include "Carro.hpp"
 
@@ -7,19 +12,49 @@
 #include <string>
 #include <vector>
 
+/**
+ * @class Locadora
+ * @brief Classe que representa uma locadora de carros
+ */
 class Locadora {
 private:
     //std::vector<Carro> carros;
 
 public:
-    std::vector<Carro> carros;
+    std::vector<Carro> carros; /**< Vetor de carros disponíveis na locadora */
+    
+    /**
+     * @brief Adiciona um carro à locadora
+     * @param carro O carro a ser adicionado
+     */
     void adicionarCarro(Carro carro);
+    
+    /**
+     * @brief Lista os carros disponíveis na locadora
+     */
     void listarCarrosDisponiveis();
+    
+    /**
+     * @brief Aluga um carro pelo modelo
+     * @param modelo O modelo do carro a ser alugado
+     */
     void alugarCarro(std::string modelo);
+    
+    /**
+     * @brief Devolve um carro alugado pelo modelo
+     * @param modelo O modelo do carro a ser devolvido
+     */
     void devolverCarro(std::string modelo);
 
+    /**
+     * @brief Construtor padrão da classe Locadora
+     */
     Locadora();
+    
+    /**
+     * @brief Destrutor da classe Locadora
+     */
     ~Locadora();
 };
 
-#endif
+#endif // LOCADORA_HPP
