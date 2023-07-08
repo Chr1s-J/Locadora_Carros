80112c640bf2bd81a4586da7811658452ca2c9a7
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-07 21:55:04 -0300

    Update Admin.hpp

--- Admin.hpp
+++ Admin.hpp
@@ -1,5 +1,10 @@
-#ifndef ADMIN_H
-#define ADMIN_H
+/**
+ * @file Admin.hpp
+ * @brief Definição da classe Admin
+ */
+
+#ifndef ADMIN_HPP
+#define ADMIN_HPP
 
 #include "Conta.hpp"
 #include "Veiculo.hpp"
@@ -9,16 +14,33 @@
 #include <string>
 #include <vector>
 
+/**
+ * @class Admin
+ * @brief Classe que representa um administrador
+ */
+
 class Admin : public Conta {
 private:
-    static const std::string MASTER_USER;
-    static const std::string MASTER_SENHA;
+    static const std::string MASTER_USER; /**< Nome de usuário do administrador master */
+    static const std::string MASTER_SENHA; /**< Senha do administrador master */
 
 public:
+    /**
+     * @brief Construtor padrão da classe Admin
+     */
     Admin();
+    /**
+     * @brief Destrutor da classe Admin
+     */
     ~Admin();
 
+    /**
+     * @brief Verifica se o usuário e a senha correspondem ao administrador master
+     * @param user O nome de usuário a ser verificado
+     * @param senha A senha a ser verificada
+     * @return true se o usuário e a senha correspondem ao administrador master, false caso contrário
+     */
     static bool isMasterAdmin(const std::string& user, const std::string& senha);
 };
 
-#endif
+#endif // ADMIN_HPP
