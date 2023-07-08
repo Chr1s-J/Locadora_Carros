b6f386bb3b66ed1f26ac9c5cd00704ad1ab97224
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-07 21:56:02 -0300

    Update Carro.hpp

--- Carro.hpp
+++ Carro.hpp
@@ -1,5 +1,10 @@
-#ifndef CARRO_H
-#define CARRO_H
+/**
+ * @file Carro.hpp
+ * @brief Definição da classe Carro
+ */
+
+#ifndef CARRO_HPP
+#define CARRO_HPP
 
 
 #include <iostream>
@@ -7,21 +12,36 @@
 
 #include "Veiculo.hpp"
 
+/**
+ * @class Carro
+ * @brief Classe que representa um carro
+ */
 class Carro : public Veiculo{
 private:
     
 public:
-    bool arCondicionado;
-    bool marchaAutomatica;
-    bool motorFlex;
-    bool disponivel = true;
+    bool arCondicionado; /**< Indica se o carro possui ar condicionado */
+    bool marchaAutomatica; /**< Indica se o carro cambio automático */
+    bool motorFlex; /**< Indica se o carro possui motor flex */
+    bool disponivel = true; /**< Indica se o carro está disponível para locação */
 
 
-
+    /**
+     * @brief Construtor da classe Carro
+     * @param modelo O modelo do carro
+     * @param marca A marca do carro
+     * @param ano O ano do carro
+     * @param placa A placa do carro
+     * @param valorDiaria O valor da diária do carro
+     */
     Carro(std::string modelo, std::string marca, int ano, std::string placa, double valorDiaria);
+   
+   /**
+     * @brief Destrutor da classe Carro
+     */
     ~Carro();
 };
 
 
 
-#endif
+#endif // CARRO_HPP
