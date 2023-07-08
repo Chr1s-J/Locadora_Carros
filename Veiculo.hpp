039c216d6ba2c8978f98d6fae551192023bc46e6
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-07 22:00:00 -0300

    Update Veiculo.hpp

--- Veiculo.hpp
+++ Veiculo.hpp
@@ -1,25 +1,43 @@
-#ifndef VEICULO_H
-#define VEICULO_H
+/**
+ * @file Veiculo.hpp
+ * @brief Definição da classe Veiculo
+ */
+
+#ifndef VEICULO_HPP
+#define VEICULO_HPP
 
 #include <iostream>
 #include <string>
 
+/**
+ * @class Veiculo
+ * @brief Classe que representa um veículo
+ */
 class Veiculo{
 private:
-    
+    // Nenhum atributo privado
+
 public:
+    /**
+     * @brief Construtor da classe Veiculo
+     */
     Veiculo();
+
+    /**
+     * @brief Destrutor da classe Veiculo
+     */
     ~Veiculo();
-    std::string marca;
-    std::string tipo;
-    std::string modelo;
-    std::string cor;
-    std::string placa;
-    int ano;
-    double valorDiaria;
+
+    std::string marca; /**< A marca do veículo */
+    std::string tipo; /**< O tipo do veículo */
+    std::string modelo; /**< O modelo do veículo */
+    std::string cor; /**< A cor do veículo */
+    std::string placa; /**< A placa do veículo */
+    int ano; /**< O ano do veículo */
+    double valorDiaria; /**< O valor da diária do veículo */
 
 };
 
 
 
-#endif
+#endif // VEICULO_HPP
