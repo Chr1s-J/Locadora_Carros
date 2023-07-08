a55f948ca6ce80b2a0bf6c078e6dbc7b1ae2c6a7
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-07 21:59:15 -0300

    Update Pagamento.hpp

--- Pagamento.hpp
+++ Pagamento.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file Pagamento.hpp
+ * @brief Definição da classe Pagamento
+ */
+
 #ifndef PAGAMENTO_HPP
 #define PAGAMENTO_HPP
 
@@ -5,18 +10,43 @@
 #include "Locadora.hpp"
 #include "Carro.hpp"
 
+/**
+ * @class Pagamento
+ * @brief Classe que representa o pagamento referente ao aluguel do carro
+ */
 class Pagamento {
 private:
-    Carro* carro;
-    Locadora* locadora;
-    //const double taxa = 80.0; // Taxa constante
+    Carro* carro; /**< Ponteiro para o carro a ser pago */
+    Locadora* locadora; /**< Ponteiro para a locadora */
+
 
 public:
-    const double taxa = 80.0; // Taxa constante
+    const double taxa = 80.0; /**< Taxa constante */
+
+    /**
+     * @brief Construtor da classe Pagamento
+     * @param _locadora Ponteiro para a locadora
+     * @param _carro Ponteiro para o carro a ser pago
+     */
     Pagamento(Locadora* _locadora, Carro* _carro);
+
+    /**
+     * @brief Construtor padrão da classe Pagamento
+     */
     Pagamento();
+
+    /**
+     * @brief Destrutor da classe Pagamento
+     */
     ~Pagamento();
+
+    /**
+     * @brief Efetua o pagamento de um carro alugado
+     * @param modelo O modelo do carro a ser pago
+     * @param cpf O CPF do cliente
+     * @param dias A quantidade de dias de aluguel
+     */
     void efetuarPagamento(std::string modelo, int cpf, int dias);
 };
 
-#endif
+#endif // PAGAMENTO_HPP
